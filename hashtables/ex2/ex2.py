8cb338c524ae152b300abfffc78893704815c316
Elysia Gabe <elysiagabe@gmail.com>
2020-08-07 08:35:00 -0700

    Completes ex2.py problem (1 of 3)

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,25 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # initialize source-destination hashtable
+    trip_dict = {}
+
+    # iterate over tickets array...each ticket in the array should become an entry in trip_dict, where source is the key & destination is the value
+    for ticket in tickets: 
+        trip_dict[ticket.source] = ticket.destination
+
+    # initialize route list...this will be output of destinations in order
+    route = [None] * length
+    i = 0
+    # loop thru & add destinations to route @ index in order
+    while i in range(length):
+        if route[0] is None: 
+            airport = trip_dict["NONE"]
+            route[i] = airport
+        else:
+            airport = trip_dict[route[i-1]]
+            route[i] = airport
+        i += 1
 
     return route
+
